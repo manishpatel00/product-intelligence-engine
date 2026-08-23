40c223c14a42e520c9890e79dbf37125bc3d9541
MANISH KUMAR <146671113+manishpatel00@users.noreply.github.com>
2026-08-24 02:29:48 +0530

    collapse sidebar on dashboard launch

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.set_page_config(
     page_title="Unilog Product Intelligence Engine",
     page_icon="⚙️",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
 )
 
 # ---------------------------------------------------------------------------
